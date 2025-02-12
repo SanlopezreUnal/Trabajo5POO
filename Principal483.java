b98b1409f23cc2cfc3aa7a298493d0aaa4641260
SanlopezreUnal <sanlopezre@unal.edu.co>
2025-02-11 23:19:12 -0500

    Add files via upload

---
+++ Principal483.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.principal483;
+
+/**
+ *
+ * @author USER
+ */
+public class Principal483 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+        
+        
+    } 
+}
